bf3b83cb3747854ee30c8a579b02424622088c4a
Scott Morrison <scott.morrison@gmail.com>
2022-10-06 19:38:14 +1100

    update

--- Mathlib/Data/HashMap.lean
+++ Mathlib/Data/HashMap.lean
@@ -1,24 +1,36 @@
+/-
+Copyright (c) 2022 Scott Morrison. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Scott Morrison
+-/
 import Std.Data.HashMap
 import Mathlib.Algebra.Group.Basic
 
+/-!
+# Algebraic structures on `HashMap`.
+
+-/
+
 open Std
 
-variable [BEq α] [Hashable α] [AddMonoid β] [DecidableEq β]
+variable [BEq α] [Hashable α] [DecidableEq β]
 
 namespace Std.HashMap
 
-@[ext]
-lemma ext {f g : HashMap α β} (w : ∀ a, f.find? a = g.find? a) : f = g := sorry
-
-lemma ext_findD {f g : HashMap α β} (w : ∀ a, f.findD a 0 = g.findD a 0) : f = g := sorry
-
-def add (f g : HashMap α β) : HashMap α β :=
+/-- Pointwise addition of `HashMap`s. -/
+def add [Zero β] [Add β] (f g : HashMap α β) : HashMap α β :=
   f.fold (fun h a b =>
     let b' := b + h.findD a 0
     if b' = 0 then h.erase a else h.insert a b') g
 
+@[ext]
+lemma ext {f g : HashMap α β} (w : ∀ a, f.find? a = g.find? a) : f = g := sorry
+
+lemma ext_findD [Zero β] {f g : HashMap α β} (w : ∀ a, f.findD a 0 = g.findD a 0) : f = g := sorry
+
 @[simp]
-lemma findD_add (f g : HashMap α β) (a : α) : (f.add g).findD a 0 = f.findD a 0 + g.findD a 0 :=
+lemma findD_add [Zero β] [Add β] (f g : HashMap α β) (a : α) :
+    (f.add g).findD a 0 = f.findD a 0 + g.findD a 0 :=
   sorry
 
 instance [AddMonoid β] : AddMonoid (HashMap α β) :=
