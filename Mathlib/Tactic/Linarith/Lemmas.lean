872a83c425852233a144a1d9344989b9b36b56ad
Scott Morrison <scott.morrison@gmail.com>
2022-10-07 15:07:04 +1100

    working on OrderedRing

--- Mathlib/Tactic/Linarith/Lemmas.lean
+++ Mathlib/Tactic/Linarith/Lemmas.lean
@@ -1,20 +1,20 @@
 import Std.Tactic.Simpa
 import Std.Tactic.Lint.Basic
-import Mathlib.Algebra.Ring.Basic
+import Mathlib.Algebra.Order.Ring
 
 namespace Linarith
 
--- theorem mul_neg {α} [OrderedRing α] {a b : α} (ha : a < 0) (hb : 0 < b) : b * a < 0 :=
--- have : (-b)*a > 0 := mul_pos_of_neg_of_neg (neg_neg_of_pos hb) ha
--- neg_of_neg_pos (by simpa)
+theorem mul_neg {α} [OrderedRing α] {a b : α} (ha : a < 0) (hb : 0 < b) : b * a < 0 :=
+have : (-b)*a > 0 := mul_pos_of_neg_of_neg (neg_neg_of_pos hb) ha
+neg_of_neg_pos (by simpa)
 
--- theorem mul_nonpos {α} [OrderedRing α] {a b : α} (ha : a ≤ 0) (hb : 0 < b) : b * a ≤ 0 :=
--- have : (-b)*a ≥ 0 := mul_nonneg_of_nonpos_of_nonpos (le_of_lt (neg_neg_of_pos hb)) ha
--- by simpa
+theorem mul_nonpos {α} [OrderedRing α] {a b : α} (ha : a ≤ 0) (hb : 0 < b) : b * a ≤ 0 :=
+have : (-b)*a ≥ 0 := mul_nonneg_of_nonpos_of_nonpos (le_of_lt (neg_neg_of_pos hb)) ha
+by simpa
 
--- -- used alongside `mul_neg` and `mul_nonpos`, so has the same argument pattern for uniformity
--- @[nolint unusedArguments]
--- theorem mul_eq {α} [OrderedSemiring α] {a b : α} (ha : a = 0) (hb : 0 < b) : b * a = 0 :=
--- by simp [*]
+-- used alongside `mul_neg` and `mul_nonpos`, so has the same argument pattern for uniformity
+@[nolint unusedArguments]
+theorem mul_eq {α} [OrderedSemiring α] {a b : α} (ha : a = 0) (_ : 0 < b) : b * a = 0 :=
+by simp [*]
 
 end Linarith
